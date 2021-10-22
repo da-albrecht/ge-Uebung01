3265884d7bfaf618bb568f4e102fa134c32be46a
da-albrecht <albrecht.david3@gmail.com>
2021-10-22 11:08:01 +0200

    initial commit

---
+++ Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject cubePrefab = null;
+    [Range(1, 200)]
+    [SerializeField] private int cubeSpawnAmount = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space was pressed");
+            this.SpawnXCubes(this.cubeSpawnAmount);
+        }
+    }
+
+    private void SpawnXCubes(int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject.Instantiate(this.cubePrefab);
+        }
+    }
+}
